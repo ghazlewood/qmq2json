b25d4311001a4350ab5cac9bb3978035990e945d
George Hazlewood <george@layer1.co.uk>
2013-10-20 20:20:41 +0100

    print output command to debug

--- qmq2json.php
+++ qmq2json.php
@@ -15,4 +15,5 @@ foreach($statistics as $stat => $stat_label) {
 	$result[$stat] = $qmhandle . " -s | grep \"" . $stat_label . "\" | awk -F \":\" '{print $2}' | sed 's/ //g'";
 }
 
-print json_encode(array( 'qmail_queue' =>$result) );
+//print json_encode(array( 'qmail_queue' =>$result) );
+var_dump($result);
