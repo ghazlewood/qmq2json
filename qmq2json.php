e93e05da02383766d3d483a17f887204b6ee2e00
George Hazlewood <george@layer1.co.uk>
2013-10-20 19:54:18 +0100

    initial commit

---
+++ qmq2json.php
@@ -0,0 +1,18 @@
+<?php
+
+// Script to export qmail queue size to json
+
+$qmhandle = '/usr/bin/qmhandle.pl';
+
+$statistics = array(
+	'total' => 'Total messages',
+	'local' => 'Messages with local recipients',
+	'remote' => 'Messages with remote recipients'
+);
+
+
+foreach($statistics as $stat => $stat_label) {
+	$result[$stat] = exec($qmhandle . " -s " . "| grep \"" . $stat_label . "\" | awk -F \":\" '{print $2}' | sed 's/ //g'");
+}
+
+print json_encode(array( 'qmail_queue' =>$result) );
